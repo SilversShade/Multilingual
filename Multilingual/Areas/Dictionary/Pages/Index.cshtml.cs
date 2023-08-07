4b22c088c9611cba7c9d59e68ab5f2287f0835a3
Alex <shantemgardenix@gmail.com>
2023-08-07 22:12:29 +0500

    Dictionary's main page now uses Authorize attribute instead of a manual authorization verification

--- Multilingual/Areas/Dictionary/Pages/Index.cshtml.cs
+++ Multilingual/Areas/Dictionary/Pages/Index.cshtml.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Multilingual.Areas.Dictionary.Pages;
 
+[Authorize]
 public class Index : PageModel
 {
-    public IActionResult OnGet()
+    public void OnGet()
     {
-        if (User.Identity is {IsAuthenticated: true})
-            return Page();
-        return RedirectToPage("/Account/Login", new {area = "Identity"});
     }
 }
